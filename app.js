9565f7e8530348793b42906827d0cdc7ec6bd709
truman <trumanwu1998@gmail.com>
2025-03-09 22:54:44 +0700

    启动默认设置为dev，并加载配置

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+// server.js
+const path = require("path");
+const dotenv = require("dotenv");
+
+// 如果没有设置 NODE_ENV，就默认为 "development"
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = "dev";
+}
+
+// 根据 NODE_ENV 拼出要加载的文件名
+const envFile = `.env.${process.env.NODE_ENV}`;
+dotenv.config({ path: path.resolve(__dirname, envFile) });
+
+console.log("Loaded ENV file:", envFile, "NODE_ENV:", process.env.NODE_ENV);
+
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
