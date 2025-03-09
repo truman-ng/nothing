6524f7fac47c46da0a50a2f4d5db8a05459313f1
truman <trumanwu1998@gmail.com>
2025-03-09 22:47:10 +0700

    更新pg和redis的二套配置

--- config/db.js
+++ config/db.js
@@ -7,7 +7,11 @@ const pool = new Pool({
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
     port: process.env.PG_PORT || 5432,
-    max: 10
+
+    // 关键参数：最大连接数
+    max: process.env.PG_MAX_CONNECTION || 10,                // 最大并发客户端数 (默认10)
+    idleTimeoutMillis: 30000 // 空闲连接超时时间(毫秒)，默认30000
+    // connectionTimeoutMillis: 2000 // 连接建立超时时间(毫秒)，默认不限制
 });
 
 pool.on("connect", () => {
