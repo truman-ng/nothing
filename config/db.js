833eb91b8e39799590e84a519f4eb5a081475d80
truman <trumanwu1998@gmail.com>
2025-03-09 23:39:12 +0700

    chore(config): 更新本地数据库配置信息
    修改了数据库连接地址和认证参数，以匹配最新的本地开发环境需求。

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
     host: process.env.PG_HOST,
     user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
+    password: process.env.PG_PASSWORD || 10,
     database: process.env.PG_DATABASE,
     port: process.env.PG_PORT || 5432,
 
