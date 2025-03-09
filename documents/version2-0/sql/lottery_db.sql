d1f2e11c9005474e85420fc7e05cab8cb011116b
truman <trumanwu1998@gmail.com>
2025-03-09 19:40:26 +0700

    更改数据库，新增sql文件记录

---
+++ documents/version2-0/sql/lottery_db.sql
@@ -0,0 +1,37 @@
+create
+database lottery_db
+    with owner neondb_owner;
+\c
+lottery_db;
+
+-- 创建 users 表
+CREATE TABLE IF NOT EXISTS users
+(
+    user_id
+    SERIAL
+    PRIMARY
+    KEY,
+    username
+    VARCHAR
+(
+    20
+) NOT NULL UNIQUE,
+    nickname VARCHAR
+(
+    30
+) NOT NULL,
+    password VARCHAR
+(
+    255
+) NOT NULL,
+    email VARCHAR
+(
+    100
+) NOT NULL UNIQUE,
+    register_time TIMESTAMP NOT NULL,
+    login_time TIMESTAMP DEFAULT NULL,
+    signature VARCHAR
+(
+    600
+) DEFAULT NULL
+    );
